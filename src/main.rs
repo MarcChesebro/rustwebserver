5c9221f5b37f0665bd79a50420b8bde1df903663
Marc Chesebro <marc.chesebro@dematic.com>
2019-08-29 13:52:50 -0400

    added multithreading

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,19 @@ use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
 use std::fs;
+use rust_webserver::ThreadPool;
 
 fn main() {
     // set up a TCP listener on port 7878
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(6);
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handle_connection(stream);
+        pool.execute(|| {
+            handle_connection(stream);
+        });
     }
 }
 
@@ -19,9 +23,18 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    let contents = fs::read_to_string("hello.html").unwrap();
+    let index_get = b"GET / HTTP/1.1\r\n";
 
-    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
+    let (status_line, filename) = if buffer.starts_with(index_get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+    let headers = format!("Content-Length: {}", contents.len());
+
+    let response = format!("{}\r\n{}\r\n\r\n{}", status_line, headers, contents);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
